00571e41756475164f77c387153b6a0f4f49a15e
Chris Hundhausen <hundhaus@wsu.edu>
2020-09-17 20:13:03 -0700

    Updated script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,7 +1,5 @@
-class Hello extends React.Component {
-    render() {
-      return React.createElement('h1', null, 'Hello World');
-    }
+const Hello = () => {
+  return React.createElement('h1', null, 'Hello World');
 }
   
 ReactDOM.render(
