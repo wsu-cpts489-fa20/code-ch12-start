d6e834df78425c74ba60464a929de140a921e94f
Chris Hundhausen <hundhaus@wsu.edu>
2020-09-19 11:31:55 -0700

    Initial commit for Ch. 11 code

--- js/script1.js
+++
@@ -1,8 +0,0 @@
-const Hello = () => {
-  return React.createElement('h1', null, 'Hello World');
-}
-  
-ReactDOM.render(
-    React.createElement(Hello),
-    document.getElementById('root')
-  );
--- js/script2.js
+++
@@ -1,8 +0,0 @@
-const Hello = () => {
-    return <h1>Hello World</h1>;
-}
-
-ReactDOM.render(
-<Hello />,
-document.getElementById('root')
-);
