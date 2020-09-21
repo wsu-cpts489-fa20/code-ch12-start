47d8ac3ce7793dd2d6dc1fb24417b78310a17154
Chris Hundhausen <hundhaus@wsu.edu>
2020-09-21 10:43:28 -0700

    FormattedTime integrated into Clock component but clock does not tick.

--- js/clock.js
+++ js/clock.js
@@ -1,38 +1,42 @@
+//A React Component for displaying the time in a 
+//human-readable textual format.
+class FormattedTime extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: props.date};
+  }
+  render() {
+    return this.state.date.toLocaleTimeString();
+  }
+}
+
+//A React component for displaying a ticking clock.
 class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
+  constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+  }
+  //Update Clock's state to the current time
+  updateTime() {
+      this.setState({date: new Date()});
     }
-
-    updateTime() {
-        this.setState({
-          date: new Date()
-        });
-      }
-
-
-    componentDidMount() {
-        this.timer = setInterval(
-          () => this.updateTime(),
-          1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer);
-    }
-
-    render() {
-      return (
-        <div>
-          <h1>Hello CptS 489 Students!</h1>
-          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        </div>
-      );
-    }
+  //Set class variable 'timer' to a function that calls updateTime() every second
+  componentDidMount() {
+      this.timer = setInterval(() => this.updateTime(), 1000);
+  }
+  componentWillUnmount() {
+      clearInterval(this.timer);
+  }
+  render() {
+    return (
+      <div><h1>Hello CptS 489 Students!</h1>
+        <h2>It is <FormattedTime date={this.state.date} />.</h2>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-    <Clock />,
-    document.getElementById('root')
+  <Clock />,
+  document.getElementById('root')
 );
