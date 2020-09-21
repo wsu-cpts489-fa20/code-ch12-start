63e66a3e6613a4dff9e9a97857e15973fb7cd970
Chris Hundhausen <hundhaus@wsu.edu>
2020-09-21 10:45:04 -0700

    FormattedTime with getDerivedStateFromProps Clock now ticks!

--- js/clock.js
+++ js/clock.js
@@ -5,6 +5,15 @@ class FormattedTime extends React.Component {
     super(props);
     this.state = {date: props.date};
   }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.date != prevState.date) {
+      return {date: nextProps.date};
+    }  else {
+      return null;
+    }
+  }
+
   render() {
     return this.state.date.toLocaleTimeString();
   }
